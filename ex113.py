c3ada431d404e715295e29cf0d13b47c17a31c3f
ErickJonesA7X <jonescomercial@gmail.com>
2020-04-19 06:09:22 -0300

    Finalizando Mundo 3 Python

---
+++ ex113.py
@@ -0,0 +1,31 @@
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mErro! Digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mErro! Digite um número real válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+#Programa Principal
+n1 = leiaint('Digite um número: ')
+n2 = leiaFloat('Digite um real: ')
+print(f'O valor inteiro digitado foi {n1} e o real foi {n2}.')
---
+++ ex115/sistema.py
@@ -0,0 +1,25 @@
+from ex115.lib.interface import *
+from time import sleep
+from ex115.lib.arquivo import *
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resposta == 1:
+        #Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
